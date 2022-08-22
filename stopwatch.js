3874d9fbb04eeae534dc6c67e9be11017e409495
ZeSved <timothy.korell@gmail.com>
2022-08-22 22:10:41 +0200

    Added unfinished stopwatch

---
+++ stopwatch.js
@@ -0,0 +1,30 @@
+const start = document.querySelector('.start')
+const stop = document.querySelector('.stop')
+const tim = document.querySelector('.time')
+const ms = document.querySelector('.ms')
+
+const time = new Date()
+const t = {
+  hours: time.getHours(),
+  minutes: time.getMinutes(),
+  seconds: time.getSeconds(),
+  milliseconds: time.getMilliseconds(),
+}
+
+start.addEventListener('click', () => {
+  setInterval(() => {
+    const time = new Date()
+    const t = {
+      hours: time.getHours(),
+      minutes: time.getMinutes(),
+      seconds: time.getSeconds(),
+      milliseconds: time.getMilliseconds(),
+    }
+    tim.innerText = `${t.hours}:${t.minutes}:${t.seconds}`
+    ms.innerText = t.milliseconds
+  }, 1)
+})
+
+stop.addEventListener('click', () => {
+  clearInterval()
+})
