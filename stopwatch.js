53d48175021626da189ab81057bd39c1cd0e4953
ZeSved <timothy.korell@gmail.com>
2022-09-07 20:58:14 +0200

    temporarily stopped development on watch

--- stopwatch.js
+++ stopwatch.js
@@ -1,22 +1,35 @@
 const start = document.querySelector('.start')
 const stop = document.querySelector('.stop')
-const tim = document.querySelector('.time')
-const ms = document.querySelector('.ms')
+const reset = document.querySelector('.reset')
 
-start.addEventListener('click', () => {
-  setInterval(() => {
-    const time = new Date()
-    const t = {
-      hours: time.getHours(),
-      minutes: time.getMinutes(),
-      seconds: time.getSeconds(),
-      milliseconds: time.getMilliseconds(),
-    }
-    tim.innerText = `${t.hours}:${t.minutes}:${t.seconds}`
-    ms.innerText = t.milliseconds
-  }, 1)
-})
+const HH = document.getElementById('HH')
+const MM = document.getElementById('MM')
+const SS = document.getElementById('SS')
+const MS = document.getElementById('MS')
+let timer
+let ms_sum = 0
+let beginDate = new Date()
 
-stop.addEventListener('click', () => {
-  clearInterval()
+const handleIncrement = () => {
+
+}
+const handleUpdate = () => {
+  //HH.innerText = `${ms_sum / 1000*60*60 % 60}`
+  //MM.innerText = `${ms_sum / 1000*60    % 60}`
+  SS.innerText = `${ms_sum}`
+  //MS.innerText = `${ms_sum % 1000}`
+}
+
+start.addEventListener('click', () => timer = setInterval(() => {
+  handleIncrement()
+  handleUpdate()
+}, 1))
+stop.addEventListener('click', () => clearInterval(timer))
+reset.addEventListener('click', () => {
+  if(timer){
+    clearInterval(timer)
+    ms_sum = 0
+  }else{
+    ms_sum = 0
+  }
 })
